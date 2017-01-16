3705575cbd34261299ef7700f08b6d897928096f
josmanuelsolano <josmanuelsolano@gmail.com>
2017-01-15 22:40:50 -0800

    add more operation after equals pressed

--- js/calcJQuery.js
+++ js/calcJQuery.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
     var actualElementValue = '';
     var numbers = [];
     var operators = [];
+    var memory = 0;
 
     function myCalculator(keyCode){
         var keyValue = '';
         switch (keyCode) {
             case 67://C
                 resetValues();
+                clearScreens();
                 break;
             case 83://S
                 if(actualElementValue.charAt(0) == '-'){
@@ -19,100 +21,32 @@ $(document).ready(function(){
                 }
                 break;
             case 82://R
-                if (numbers.length == 0 && $('#screen-bottom').html().length == 0) {
-                    alert('you can´t do that oparation without a first element');
-                }else {
-                    numbers.push($('#screen-bottom').html());
-                    actualElementValue = '';
-                    operators.push('root');
-                    $('#screen-top').append($('#screen-bottom').html() + ' root ');
-                    $('#screen-bottom').html('');
-                }
-                console.log(numbers);
-                console.log(operators);
+                operationSign('root');
                 break;
             case 88://x
-                if (numbers.length == 0 && $('#screen-bottom').html().length == 0) {
-                    numbers.push(0);
-                    operators.push('*');
-                    $('#screen-top').append('0' + ' * ');
-                    actualElementValue += '';
-                    $('#screen-bottom').html('');
-                }else {
-                    numbers.push(parseFloat($('#screen-bottom').html()));
-                    actualElementValue = '';
-                    operators.push('*');
-                    $('#screen-top').append($('#screen-bottom').html() + ' * ');
-                    $('#screen-bottom').html('');
-                }
-                console.log(numbers);
-                console.log(operators);
+                operationSign('*');
                 break;
             case 68:///
-                if (numbers.length == 0 && $('#screen-bottom').html().length == 0) {
-                    numbers.push(0);
-                    operators.push('/');
-                    $('#screen-top').append('0' + ' / ');
-                    actualElementValue += '';
-                    $('#screen-bottom').html('');
-                }else {
-                    numbers.push(parseFloat($('#screen-bottom').html()));
-                    actualElementValue = '';
-                    operators.push('/');
-                    $('#screen-top').append($('#screen-bottom').html() + ' / ');
-                    $('#screen-bottom').html('');
-                }
-                console.log(numbers);
-                console.log(operators);
+                operationSign('/');
                 break;
             case 187://+
-                if (numbers.length == 0 && $('#screen-bottom').html().length == 0) {
-                    numbers.push(0);
-                    operators.push('+');
-                    $('#screen-top').append('0' + ' + ');
-                    actualElementValue += '';
-                    $('#screen-bottom').html('');
-                }else {
-                    numbers.push(parseFloat($('#screen-bottom').html()));
-                    actualElementValue = '';
-                    operators.push('+');
-                    $('#screen-top').append($('#screen-bottom').html() + ' + ');
-                    $('#screen-bottom').html('');
-                }
-                console.log(numbers);
-                console.log(operators);
+                operationSign('+');
                 break;
             case 189://-
-                if (numbers.length == 0 && $('#screen-bottom').html().length == 0) {
-                    numbers.push(0);
-                    operators.push('-');
-                    $('#screen-top').append('0' + ' - ');
-                    actualElementValue += '';
-                    $('#screen-bottom').html('');
-                }else {
-                    numbers.push(parseFloat($('#screen-bottom').html()));
-                    actualElementValue = '';
-                    operators.push('-');
-                    $('#screen-top').append($('#screen-bottom').html() + ' - ');
-                    $('#screen-bottom').html('');
-                }
-                console.log(numbers);
-                console.log(operators);
+                operationSign('-');
                 break;
             case 13://=
                 if ($('#screen-bottom').html() == '' && numbers.length != 0) {
                     operators.pop();
-                    console.log(numbers);
-                    console.log(operators);
                     resolve();
+                    $('#screen-top').html('');
+                    resetValues();
                 } else {
                     if ($('#screen-bottom').html() != '') {
                         numbers.push(parseFloat($('#screen-bottom').html()));
-                        $('#screen-top').append($('#screen-bottom').html());
-                        $('#screen-bottom').html('');
-                        console.log(numbers);
-                        console.log(operators);
                         resolve();
+                        $('#screen-top').html('');
+                        resetValues();
                     }
 
                 }
@@ -120,13 +54,16 @@ $(document).ready(function(){
             case 116://F5
                 break;
             case 118://F7
+                $('#screen-bottom').html(memory)
                 break;
             case 119://F8
+                memory = 0;
                 break;
             case 120://F9
+                memory += parseFloat($('#screen-bottom').html());
                 break;
             case 190://dot
-                if (validateDot(actualElementValue)) {
+                if (validateDot($('#screen-bottom').html())) {
                     $('#screen-bottom').append('.');
                 }else {
                     alert("You can't capture more than two dots into a number");
@@ -135,7 +72,6 @@ $(document).ready(function(){
             default:
                 keyValue = String.fromCharCode(keyCode);
                 $("#screen-bottom").append(actualElementValue.concat(keyValue));
-                console.log(actualElementValue);
 
         }
 
@@ -162,12 +98,46 @@ $(document).ready(function(){
                     result = numbers.shift() / numbers.shift();
                     numbers.unshift(result);
                     break;
+                case 'root':
+                    var root = numbers.shift();
+                    var n = numbers.shift();
+                    result = Math.pow(n, 1/root);
+                    numbers.unshift(result);
+                    break;
                 default:
 
             }
 
         }
         $('#screen-bottom').html(numbers.shift());
+    }
+
+    function operationSign(sign){
+        if (sign === 'root') {
+            if (numbers.length == 0 && $('#screen-bottom').html().length == 0) {
+                alert('you can´t do that oparation without a first element');
+            }else {
+                numbers.push(parseFloat($('#screen-bottom').html()));
+                actualElementValue = '';
+                operators.push(sign);
+                $('#screen-top').append($('#screen-bottom').html() + ' '+ sign + ' ');
+                $('#screen-bottom').html('');
+            }
+        } else {
+            if (numbers.length == 0 && $('#screen-bottom').html().length == 0) {
+                numbers.push(0);
+                operators.push(sign);
+                $('#screen-top').append('0' + ' '+ sign + ' ');
+                actualElementValue += '';
+                $('#screen-bottom').html('');
+            }else {
+                numbers.push(parseFloat($('#screen-bottom').html()));
+                actualElementValue = '';
+                operators.push(sign);
+                $('#screen-top').append($('#screen-bottom').html() + ' '+ sign + ' ');
+                $('#screen-bottom').html('');
+            }
+        }
     }
 
     function validateDot(number){
@@ -178,9 +148,12 @@ $(document).ready(function(){
         }
     }
 
-    function resetValues(){
+    function clearScreens(){
         $('#screen-top').html('');
         $('#screen-bottom').html('');
+    }
+
+    function resetValues(){
         actualElementValue = '';
         numbers = [];
         operators = [];
@@ -204,4 +177,24 @@ $(document).ready(function(){
             myCalculator(e.which);
         }
     });
+
+    $('#one,#two,#three,#four,#five,'
+     +'#six,#seven,#eight,#nine,#zero,'
+     +'#dot,#sign,#divide,#multiply,'
+     +'#minus,#plus,#root,#equals,#clear,'
+     +'#del,#mrecall,#mclear,#mplus').click(function(){
+        var codes = {
+            zero:48, one:49, two:50,
+            three:51, four:52, five:53,
+            six:54, seven:55, eight:56,
+            nine:57, dot:190, sign:83,
+            divide:68, multiply:88, minus:189,
+            plus:187, root:82, equals:13, clear:67,
+            mrecall:118, mclear:119, mplus:120
+        };
+        //$('#screen-bottom').append($(this).html());
+        myCalculator(codes[$(this).attr('id')]);
+		//myCalculator(numbersCodes[key]);
+	});
+
 });
